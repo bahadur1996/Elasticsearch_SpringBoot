46b5d2d179478c5c53f097faa5d294280ef92a0c
bahadur <bahadur@revechat.com>
2022-08-27 22:58:59 +0600

    Project added to git

---
+++ src/main/java/com/example/elasticsearch_springboot/ElasticsearchSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.elasticsearch_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticsearchSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticsearchSpringBootApplication.class, args);
+    }
+
+}
