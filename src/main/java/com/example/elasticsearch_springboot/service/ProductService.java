83ea373c6033d2c69ec278cdd9682d64cb86f99a
bahadur <bahadur@revechat.com>
2022-08-29 09:12:15 +0600

    Repository and Service added

---
+++ src/main/java/com/example/elasticsearch_springboot/service/ProductService.java
@@ -0,0 +1,23 @@
+package com.example.elasticsearch_springboot.service;
+
+import com.example.elasticsearch_springboot.documents.Product;
+import com.example.elasticsearch_springboot.repositories.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public void createProductIndexBulk(final List<Product> products) {
+        productRepository.saveAll(products);
+    }
+
+    public void createProductIndex(final Product product) {
+        productRepository.save(product);
+    }
+}
